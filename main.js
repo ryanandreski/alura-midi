b135c3f9db4194774aee6cefbe403660d77c94f3
Ryan <117860793+ryanandreski@users.noreply.github.com>
2023-10-19 09:28:28 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+document.querySelector('.tecla_pom')
+document.querySelector('.tecla_clap')
+document.querySelector('.tecla_tim')
+document.querySelector('.tecla_puff')
+document.querySelector('.tecla_splash')
+document.querySelector('.tecla_toim')
+document.querySelector('.tecla_psh')
+document.querySelector('.tecla_tic')
+document.querySelector('.tecla_tom')
+
+//function play
+
+document.querySelector('#som_tecla_pom').play
+document.querySelector('#som_tecla_clap').play
+document.querySelector('#som_tecla_tim').play
+document.querySelector('#som_tecla_puff').play
+document.querySelector('#som_tecla_splash').play
+document.querySelector('#som_tecla_toim').play
+document.querySelector('#som_tecla_psh').play
+document.querySelector('#som_tecla_tic').play
+document.querySelector('#som_tecla_tom').play
